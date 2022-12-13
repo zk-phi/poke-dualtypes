3887eb7fa2195725a942a5fa3af18d96f49ba73f
zk-phi <zk-phi@users.noreply.github.com>
2022-12-13 13:01:09 +0900

    speed

---
+++ speed.js
@@ -0,0 +1,28 @@
+const speeds = [];
+pokemonNames.forEach((name) => {
+  const po = pokemons[name];
+  speeds.push({
+    label: name + " 最遅",
+    s: po.stats.s + 5,
+  });
+  speeds.push({
+    label: name + " 無振り",
+    s: Math.floor(po.stats.s + 31 / 2) + 5,
+  });
+  speeds.push({
+    label: name + " 準速",
+    s: Math.floor(po.stats.s + 31 / 2 + 252 / 8) + 5,
+  });
+  speeds.push({
+    label: name + " 最速",
+    s: Math.floor((Math.floor(po.stats.s + 31 / 2 + 252 / 8) + 5) * 1.1),
+  });
+  speeds.push({
+    label: name + " 準速スカーフ",
+    s: Math.floor((Math.floor(po.stats.s + 31 / 2 + 252 / 8) + 5) * 1.5),
+  });
+  speeds.push({
+    label: name + " 最速スカーフ",
+    s: Math.floor(Math.floor((Math.floor(po.stats.s + 31 / 2 + 252 / 8) + 5) * 1.1) * 1.5),
+  });
+});
