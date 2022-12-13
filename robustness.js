d58734a8d1e2f92cad798c53b215e502435942a7
zk-phi <zk-phi@users.noreply.github.com>
2022-12-13 13:01:00 +0900

    minor changes

--- robustness.js
+++ robustness.js
@@ -5,22 +5,22 @@ const bruteOptimizeHBD = ({ stats, iv, ev, n }, balance = 0.5) => {
 
   const evaluate = ({ stats, iv, ev, n }) => {
     const h = Math.floor(stats.h + iv.h / 2 + ev.h / 8) + 60;
-    const b = Math.floor((Math.floor(stats.b + iv.b / 2 + ev.b / 8) +  5) * n.b);
-    const d = Math.floor((Math.floor(stats.d + iv.d / 2 + ev.d / 8) +  5) * n.d);
+    const b = Math.floor((Math.floor(stats.b + iv.b / 2 + ev.b / 8) + 5) * n.b);
+    const d = Math.floor((Math.floor(stats.d + iv.d / 2 + ev.d / 8) + 5) * n.d);
     return { h, b, d, value: h * b * d / (2 * balance * b + (2 - 2 * balance) * d) };
   };
 
-  let maxValue = { value: 0 };
-  let maxEv = null;
-  let maxN = null;
+  let maxValue = evaluate({ stats, iv, ev, n });
+  let maxEv = ev;
+  let maxN = n;
   const limDh = Math.min(remainingEv, 252 - ev.d);
   for (n of possibleNs) {
     for (let dh = 0; dh <= limDh; dh += 4) {
       const limDb = Math.min(remainingEv - dh, 252 - ev.b);
       for (let db = 0; db <= limDb; db += 4) {
         const dd = remainingEv - dh - db;
-        if (ev.d + dd <= 252) {
-          const newEv = { h: ev.h + dh, b: ev.b + db, d: ev.d + dd };
+        if (dh + db + dd <= remainingEv && ev.d + dd <= 252) {
+          const newEv = { h: ev.h + dh, a: ev.a, b: ev.b + db, c: ev.c, d: ev.d + dd, s: ev.s };
           const value = evaluate({ stats, iv, n, ev: newEv });
           if (value.value > maxValue.value ||
               (value.value === maxValue.value && newEv.h > maxEv.h) ||
@@ -38,7 +38,8 @@ const bruteOptimizeHBD = ({ stats, iv, ev, n }, balance = 0.5) => {
 };
 
 const robustness = [];
-pokemons.forEach((po) => {
+pokemonNames.forEach((name) => {
+  const po = pokemons[name];
   const iv = { h: 31, a:31, b: 31, c: 31, d: 31, h: 31 };
   const ev508 = { h: 0, a: 0,   b: 0, c: 0, d: 0, s: 0 };
   const ev256 = { h: 0, a: 252, b: 0, c: 0, d: 0, s: 0 };
@@ -54,31 +55,31 @@ pokemons.forEach((po) => {
   const hd508  = bruteOptimizeHBD({ stats: po.stats, iv, ev: ev508, n: n   }, 1);
   const hbd508 = bruteOptimizeHBD({ stats: po.stats, iv, ev: ev508, n: n   }, 0.5);
   robustness.push({
-    label: po.name + " 4振り",
+    label: name + " 4振り",
     hb: hbMin.value.h * hbMin.value.b,
     hd: hdMin.value.h * hdMin.value.d,
     types: po.types,
   });
   robustness.push({
-    label: po.name + " 256振り (バランス)",
+    label: name + " 256振り (最適)",
     hb: hbd256.value.h * hbd256.value.b,
     hd: hbd256.value.h * hbd256.value.d,
     types: po.types,
   });
   robustness.push({
-    label: po.name + " 256振り (特化)",
+    label: name + " 256振り (特化)",
     hb: hb256.value.h * hb256.value.b,
     hd: hd256.value.h * hd256.value.d,
     types: po.types,
   });
   robustness.push({
-    label: po.name + " 508振り＋補正 (バランス)",
+    label: name + " 508振り＋補正 (最適)",
     hb: hbd508.value.h * hbd508.value.b,
     hd: hbd508.value.h * hbd508.value.d,
     types: po.types,
   });
   robustness.push({
-    label: po.name + " 508振り＋補正 (特化)",
+    label: name + " 508振り＋補正 (特化)",
     hb: hb508.value.h * hb508.value.b,
     hd: hd508.value.h * hd508.value.d,
     types: po.types,
